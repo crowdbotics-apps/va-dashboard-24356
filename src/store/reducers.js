cabd7e380fe95444006d83313872349a9713dcf0
Crowdbotics <team@crowdbotics.com>
2021-02-07 14:55:03 +0000

    Update store

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,7 +1,285 @@
 import * as types from "./constants"
 
-const initialState = {}
+const initialState = {
+  contracts: [],
+  vADashboardAPI: [],
+  customTexts: [],
+  homePages: [],
+  signups: [],
+  logins: [],
+  passwordChanges: [],
+  passwords: [],
+  passwordResetConfirms: [],
+  verifyEmails: [],
+  userDetails: []
+}
 
 export default function apiReducer(state = initialState, action) {
-    return state
+  switch (action.type) {
+    case types.API_V1_CONTRACT_LIST:
+    case types.API_V1_CONTRACT_LIST_SUCCEEDED:
+      return Object.assign({}, state, {
+        contracts: [...state.contracts, action.response]
+      })
+    case types.API_V1_CONTRACT_LIST_FAILED:
+      return Object.assign({}, state, {
+        vADashboardAPI: [...state.vADashboardAPI, action.response]
+      })
+    case types.API_V1_CONTRACT_CREATE:
+    case types.API_V1_CONTRACT_CREATE_SUCCEEDED:
+      return Object.assign({}, state, {
+        contracts: [...state.contracts, action.response]
+      })
+    case types.API_V1_CONTRACT_CREATE_FAILED:
+      return Object.assign({}, state, {
+        vADashboardAPI: [...state.vADashboardAPI, action.response]
+      })
+    case types.API_V1_CONTRACT_READ:
+    case types.API_V1_CONTRACT_READ_SUCCEEDED:
+      return Object.assign({}, state, {
+        contracts: [
+          ...state.contracts.filter(record => record.id !== action.response.id),
+          action.response
+        ]
+      })
+    case types.API_V1_CONTRACT_READ_FAILED:
+      return Object.assign({}, state, {
+        vADashboardAPI: [...state.vADashboardAPI, action.response]
+      })
+    case types.API_V1_CONTRACT_UPDATE:
+    case types.API_V1_CONTRACT_UPDATE_SUCCEEDED:
+      return Object.assign({}, state, {
+        contracts: state.contracts.map(record =>
+          record.id === action.response.id ? action.response : record
+        )
+      })
+    case types.API_V1_CONTRACT_UPDATE_FAILED:
+      return Object.assign({}, state, {
+        vADashboardAPI: [...state.vADashboardAPI, action.response]
+      })
+    case types.API_V1_CONTRACT_PARTIAL_UPDATE:
+    case types.API_V1_CONTRACT_PARTIAL_UPDATE_SUCCEEDED:
+      return Object.assign({}, state, {
+        contracts: state.contracts.map(record =>
+          record.id === action.response.id ? action.response : record
+        )
+      })
+    case types.API_V1_CONTRACT_PARTIAL_UPDATE_FAILED:
+      return Object.assign({}, state, {
+        vADashboardAPI: [...state.vADashboardAPI, action.response]
+      })
+    case types.API_V1_CONTRACT_DELETE:
+    case types.API_V1_CONTRACT_DELETE_SUCCEEDED:
+    case types.API_V1_CONTRACT_DELETE_FAILED:
+      return Object.assign({}, state, {
+        vADashboardAPI: [...state.vADashboardAPI, action.response]
+      })
+    case types.API_V1_CUSTOMTEXT_LIST:
+    case types.API_V1_CUSTOMTEXT_LIST_SUCCEEDED:
+      return Object.assign({}, state, {
+        customTexts: [...state.customTexts, action.response]
+      })
+    case types.API_V1_CUSTOMTEXT_LIST_FAILED:
+      return Object.assign({}, state, {
+        vADashboardAPI: [...state.vADashboardAPI, action.response]
+      })
+    case types.API_V1_CUSTOMTEXT_READ:
+    case types.API_V1_CUSTOMTEXT_READ_SUCCEEDED:
+      return Object.assign({}, state, {
+        customTexts: [
+          ...state.customTexts.filter(
+            record => record.id !== action.response.id
+          ),
+          action.response
+        ]
+      })
+    case types.API_V1_CUSTOMTEXT_READ_FAILED:
+      return Object.assign({}, state, {
+        vADashboardAPI: [...state.vADashboardAPI, action.response]
+      })
+    case types.API_V1_CUSTOMTEXT_UPDATE:
+    case types.API_V1_CUSTOMTEXT_UPDATE_SUCCEEDED:
+      return Object.assign({}, state, {
+        customTexts: state.customTexts.map(record =>
+          record.id === action.response.id ? action.response : record
+        )
+      })
+    case types.API_V1_CUSTOMTEXT_UPDATE_FAILED:
+      return Object.assign({}, state, {
+        vADashboardAPI: [...state.vADashboardAPI, action.response]
+      })
+    case types.API_V1_CUSTOMTEXT_PARTIAL_UPDATE:
+    case types.API_V1_CUSTOMTEXT_PARTIAL_UPDATE_SUCCEEDED:
+      return Object.assign({}, state, {
+        customTexts: state.customTexts.map(record =>
+          record.id === action.response.id ? action.response : record
+        )
+      })
+    case types.API_V1_CUSTOMTEXT_PARTIAL_UPDATE_FAILED:
+      return Object.assign({}, state, {
+        vADashboardAPI: [...state.vADashboardAPI, action.response]
+      })
+    case types.API_V1_HOMEPAGE_LIST:
+    case types.API_V1_HOMEPAGE_LIST_SUCCEEDED:
+      return Object.assign({}, state, {
+        homePages: [...state.homePages, action.response]
+      })
+    case types.API_V1_HOMEPAGE_LIST_FAILED:
+      return Object.assign({}, state, {
+        vADashboardAPI: [...state.vADashboardAPI, action.response]
+      })
+    case types.API_V1_HOMEPAGE_READ:
+    case types.API_V1_HOMEPAGE_READ_SUCCEEDED:
+      return Object.assign({}, state, {
+        homePages: [
+          ...state.homePages.filter(record => record.id !== action.response.id),
+          action.response
+        ]
+      })
+    case types.API_V1_HOMEPAGE_READ_FAILED:
+      return Object.assign({}, state, {
+        vADashboardAPI: [...state.vADashboardAPI, action.response]
+      })
+    case types.API_V1_HOMEPAGE_UPDATE:
+    case types.API_V1_HOMEPAGE_UPDATE_SUCCEEDED:
+      return Object.assign({}, state, {
+        homePages: state.homePages.map(record =>
+          record.id === action.response.id ? action.response : record
+        )
+      })
+    case types.API_V1_HOMEPAGE_UPDATE_FAILED:
+      return Object.assign({}, state, {
+        vADashboardAPI: [...state.vADashboardAPI, action.response]
+      })
+    case types.API_V1_HOMEPAGE_PARTIAL_UPDATE:
+    case types.API_V1_HOMEPAGE_PARTIAL_UPDATE_SUCCEEDED:
+      return Object.assign({}, state, {
+        homePages: state.homePages.map(record =>
+          record.id === action.response.id ? action.response : record
+        )
+      })
+    case types.API_V1_HOMEPAGE_PARTIAL_UPDATE_FAILED:
+      return Object.assign({}, state, {
+        vADashboardAPI: [...state.vADashboardAPI, action.response]
+      })
+    case types.API_V1_LOGIN_CREATE:
+    case types.API_V1_LOGIN_CREATE_SUCCEEDED:
+    case types.API_V1_LOGIN_CREATE_FAILED:
+      return Object.assign({}, state, {
+        vADashboardAPI: [...state.vADashboardAPI, action.response]
+      })
+    case types.API_V1_SIGNUP_CREATE:
+    case types.API_V1_SIGNUP_CREATE_SUCCEEDED:
+      return Object.assign({}, state, {
+        signups: [...state.signups, action.response]
+      })
+    case types.API_V1_SIGNUP_CREATE_FAILED:
+      return Object.assign({}, state, {
+        vADashboardAPI: [...state.vADashboardAPI, action.response]
+      })
+    case types.REST_AUTH_LOGIN_CREATE:
+    case types.REST_AUTH_LOGIN_CREATE_SUCCEEDED:
+      return Object.assign({}, state, {
+        logins: [...state.logins, action.response]
+      })
+    case types.REST_AUTH_LOGIN_CREATE_FAILED:
+      return Object.assign({}, state, {
+        vADashboardAPI: [...state.vADashboardAPI, action.response]
+      })
+    case types.REST_AUTH_LOGOUT_LIST:
+    case types.REST_AUTH_LOGOUT_LIST_SUCCEEDED:
+    case types.REST_AUTH_LOGOUT_LIST_FAILED:
+      return Object.assign({}, state, {
+        vADashboardAPI: [...state.vADashboardAPI, action.response]
+      })
+    case types.REST_AUTH_LOGOUT_CREATE:
+    case types.REST_AUTH_LOGOUT_CREATE_SUCCEEDED:
+    case types.REST_AUTH_LOGOUT_CREATE_FAILED:
+      return Object.assign({}, state, {
+        vADashboardAPI: [...state.vADashboardAPI, action.response]
+      })
+    case types.REST_AUTH_PASSWORD_CHANGE_CREATE:
+    case types.REST_AUTH_PASSWORD_CHANGE_CREATE_SUCCEEDED:
+      return Object.assign({}, state, {
+        passwordChanges: [...state.passwordChanges, action.response]
+      })
+    case types.REST_AUTH_PASSWORD_CHANGE_CREATE_FAILED:
+      return Object.assign({}, state, {
+        vADashboardAPI: [...state.vADashboardAPI, action.response]
+      })
+    case types.REST_AUTH_PASSWORD_RESET_CREATE:
+    case types.REST_AUTH_PASSWORD_RESET_CREATE_SUCCEEDED:
+      return Object.assign({}, state, {
+        passwords: [...state.passwords, action.response]
+      })
+    case types.REST_AUTH_PASSWORD_RESET_CREATE_FAILED:
+      return Object.assign({}, state, {
+        vADashboardAPI: [...state.vADashboardAPI, action.response]
+      })
+    case types.REST_AUTH_PASSWORD_RESET_CONFIRM_CREATE:
+    case types.REST_AUTH_PASSWORD_RESET_CONFIRM_CREATE_SUCCEEDED:
+      return Object.assign({}, state, {
+        passwordResetConfirms: [...state.passwordResetConfirms, action.response]
+      })
+    case types.REST_AUTH_PASSWORD_RESET_CONFIRM_CREATE_FAILED:
+      return Object.assign({}, state, {
+        vADashboardAPI: [...state.vADashboardAPI, action.response]
+      })
+    case types.REST_AUTH_REGISTRATION_CREATE:
+    case types.REST_AUTH_REGISTRATION_CREATE_SUCCEEDED:
+      return Object.assign({}, state, {
+        signups: [...state.signups, action.response]
+      })
+    case types.REST_AUTH_REGISTRATION_CREATE_FAILED:
+      return Object.assign({}, state, {
+        vADashboardAPI: [...state.vADashboardAPI, action.response]
+      })
+    case types.REST_AUTH_REGISTRATION_VERIFY_EMAIL_CREATE:
+    case types.REST_AUTH_REGISTRATION_VERIFY_EMAIL_CREATE_SUCCEEDED:
+      return Object.assign({}, state, {
+        verifyEmails: [...state.verifyEmails, action.response]
+      })
+    case types.REST_AUTH_REGISTRATION_VERIFY_EMAIL_CREATE_FAILED:
+      return Object.assign({}, state, {
+        vADashboardAPI: [...state.vADashboardAPI, action.response]
+      })
+    case types.REST_AUTH_USER_READ:
+    case types.REST_AUTH_USER_READ_SUCCEEDED:
+      return Object.assign({}, state, {
+        userDetails: [
+          ...state.userDetails.filter(
+            record => record.id !== action.response.id
+          ),
+          action.response
+        ]
+      })
+    case types.REST_AUTH_USER_READ_FAILED:
+      return Object.assign({}, state, {
+        vADashboardAPI: [...state.vADashboardAPI, action.response]
+      })
+    case types.REST_AUTH_USER_UPDATE:
+    case types.REST_AUTH_USER_UPDATE_SUCCEEDED:
+      return Object.assign({}, state, {
+        userDetails: state.userDetails.map(record =>
+          record.id === action.response.id ? action.response : record
+        )
+      })
+    case types.REST_AUTH_USER_UPDATE_FAILED:
+      return Object.assign({}, state, {
+        vADashboardAPI: [...state.vADashboardAPI, action.response]
+      })
+    case types.REST_AUTH_USER_PARTIAL_UPDATE:
+    case types.REST_AUTH_USER_PARTIAL_UPDATE_SUCCEEDED:
+      return Object.assign({}, state, {
+        userDetails: state.userDetails.map(record =>
+          record.id === action.response.id ? action.response : record
+        )
+      })
+    case types.REST_AUTH_USER_PARTIAL_UPDATE_FAILED:
+      return Object.assign({}, state, {
+        vADashboardAPI: [...state.vADashboardAPI, action.response]
+      })
+    default:
+      return state
+  }
 }
--- src/store/services.js
+++ src/store/services.js
@@ -1,1 +1,137 @@
 import axios from "axios"
+const vADashboardAPI = axios.create({
+  baseURL: "https://va-dashboard-24356-prod.herokuapp.com/",
+  headers: { Accept: "application/json", "Content-Type": "application/json" }
+})
+function api_v1_contract_list(action) {
+  return vADashboardAPI.get(`/api/v1/contract/`)
+}
+function api_v1_contract_create(action) {
+  return vADashboardAPI.post(`/api/v1/contract/`, null, { data: action.data })
+}
+function api_v1_contract_read(action) {
+  return vADashboardAPI.get(`/api/v1/contract/${action.id}/`)
+}
+function api_v1_contract_update(action) {
+  return vADashboardAPI.put(`/api/v1/contract/${action.id}/`, null, {
+    data: action.data
+  })
+}
+function api_v1_contract_partial_update(action) {
+  return vADashboardAPI.patch(`/api/v1/contract/${action.id}/`, null, {
+    data: action.data
+  })
+}
+function api_v1_contract_delete(action) {
+  return vADashboardAPI.delete(`/api/v1/contract/${action.id}/`)
+}
+function api_v1_customtext_list(action) {
+  return vADashboardAPI.get(`/api/v1/customtext/`)
+}
+function api_v1_customtext_read(action) {
+  return vADashboardAPI.get(`/api/v1/customtext/${action.id}/`)
+}
+function api_v1_customtext_update(action) {
+  return vADashboardAPI.put(`/api/v1/customtext/${action.id}/`, null, {
+    data: action.data
+  })
+}
+function api_v1_customtext_partial_update(action) {
+  return vADashboardAPI.patch(`/api/v1/customtext/${action.id}/`, null, {
+    data: action.data
+  })
+}
+function api_v1_homepage_list(action) {
+  return vADashboardAPI.get(`/api/v1/homepage/`)
+}
+function api_v1_homepage_read(action) {
+  return vADashboardAPI.get(`/api/v1/homepage/${action.id}/`)
+}
+function api_v1_homepage_update(action) {
+  return vADashboardAPI.put(`/api/v1/homepage/${action.id}/`, null, {
+    data: action.data
+  })
+}
+function api_v1_homepage_partial_update(action) {
+  return vADashboardAPI.patch(`/api/v1/homepage/${action.id}/`, null, {
+    data: action.data
+  })
+}
+function api_v1_login_create(action) {
+  return vADashboardAPI.post(`/api/v1/login/`)
+}
+function api_v1_signup_create(action) {
+  return vADashboardAPI.post(`/api/v1/signup/`, null, { data: action.data })
+}
+function rest_auth_login_create(action) {
+  return vADashboardAPI.post(`/rest-auth/login/`, null, { data: action.data })
+}
+function rest_auth_logout_list(action) {
+  return vADashboardAPI.get(`/rest-auth/logout/`)
+}
+function rest_auth_logout_create(action) {
+  return vADashboardAPI.post(`/rest-auth/logout/`)
+}
+function rest_auth_password_change_create(action) {
+  return vADashboardAPI.post(`/rest-auth/password/change/`, null, {
+    data: action.data
+  })
+}
+function rest_auth_password_reset_create(action) {
+  return vADashboardAPI.post(`/rest-auth/password/reset/`, null, {
+    data: action.data
+  })
+}
+function rest_auth_password_reset_confirm_create(action) {
+  return vADashboardAPI.post(`/rest-auth/password/reset/confirm/`, null, {
+    data: action.data
+  })
+}
+function rest_auth_registration_create(action) {
+  return vADashboardAPI.post(`/rest-auth/registration/`, null, {
+    data: action.data
+  })
+}
+function rest_auth_registration_verify_email_create(action) {
+  return vADashboardAPI.post(`/rest-auth/registration/verify-email/`, null, {
+    data: action.data
+  })
+}
+function rest_auth_user_read(action) {
+  return vADashboardAPI.get(`/rest-auth/user/`)
+}
+function rest_auth_user_update(action) {
+  return vADashboardAPI.put(`/rest-auth/user/`, null, { data: action.data })
+}
+function rest_auth_user_partial_update(action) {
+  return vADashboardAPI.patch(`/rest-auth/user/`, null, { data: action.data })
+}
+export const apiService = {
+  api_v1_contract_list,
+  api_v1_contract_create,
+  api_v1_contract_read,
+  api_v1_contract_update,
+  api_v1_contract_partial_update,
+  api_v1_contract_delete,
+  api_v1_customtext_list,
+  api_v1_customtext_read,
+  api_v1_customtext_update,
+  api_v1_customtext_partial_update,
+  api_v1_homepage_list,
+  api_v1_homepage_read,
+  api_v1_homepage_update,
+  api_v1_homepage_partial_update,
+  api_v1_login_create,
+  api_v1_signup_create,
+  rest_auth_login_create,
+  rest_auth_logout_list,
+  rest_auth_logout_create,
+  rest_auth_password_change_create,
+  rest_auth_password_reset_create,
+  rest_auth_password_reset_confirm_create,
+  rest_auth_registration_create,
+  rest_auth_registration_verify_email_create,
+  rest_auth_user_read,
+  rest_auth_user_update,
+  rest_auth_user_partial_update
+}
