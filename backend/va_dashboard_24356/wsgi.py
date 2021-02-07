afabb609ca35269d5169e3c6c5de62a0b3bd1c5f
Crowdbotics <team@crowdbotics.com>
2021-02-07 14:45:04 +0000

    Update models

--- backend/va_dashboard_24356/wsgi.py
+++ backend/va_dashboard_24356/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'va_dashboard_24356.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "va_dashboard_24356.settings")
 
 application = get_wsgi_application()
